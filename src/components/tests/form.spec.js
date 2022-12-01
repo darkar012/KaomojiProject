66b2a2a4dfcb4a42d1d465053b46349732439d86
IsaPinzonV <isabellapv0305@outlook.com>
2022-11-30 20:57:09 -0500

    test

--- src/components/tests/form.spec.js
+++ src/components/tests/form.spec.js
@@ -1,22 +1,29 @@
-import { mount } from "@vue/test-utils"
-import { describe, it } from "vitest"
-import form from "../form.vue"
-import calendar from "primevue/calendar"
+import { mount } from "@vue/test-utils";
+import { describe, it } from "vitest";
+import form from "../form.vue";
+import Calendar from "../../../node_modules/primevue/calendar/Calendar.vue";
 
-describe ("form component unit tests",  () => {
+describe("form component unit tests", () => {
+  it("it renders the form", () => {
+    const wrapper = mount(form);
+    expect(wrapper).toBeTruthy();
+  });
+});
 
-    it("it renders the modal", () => {
-        const wrapper = mount (form)
-        expect (wrapper).toBeTruthy()
-    })
+describe("InputImage component unit tests", () => {
+  it("it save the image", () => {
+    const wrapper = mount(form);
 
-})
+    wrapper.find("#images").trigger("change");
 
-describe ("Calendar component unit tests",  () => {
+    expect(wrapper.vm.readImage).toBeTruthy();
+  });
+});
 
-    it("it renders the modal", () => {
-        const wrapper = mount (calendar)
-        expect (wrapper).toBeTruthy()
-    })
-
-})
+describe("NewProduct method unit tests", () => {
+  it("it saves the product", () => {
+    const wrapper = mount(form);
+    wrapper.find("#images").setValue("");
+    wrapper.
+  });
+});
